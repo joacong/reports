9c775986251bace53c1e1e3b2d7f0355a0c876a1
josnunezg <josnunezg@gmail.com>
2018-07-27 12:14:42 -0400

    add redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,18 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import store from '../store';
+
+const element = document.getElementById('root');
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  element as HTMLElement
 );
 registerServiceWorker();
---
+++ store/index.ts
@@ -0,0 +1,15 @@
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { reducer } from './reducer';
+
+const logger = process.env.NODE_ENV == "development" ?
+               store => next => action => {
+                 console.log('dispatching', action);
+                 let result = next(action);
+                 console.log('new state', store.getState());
+                 return result;
+               } :
+               store => next => action => {};
+
+
+export default store = createStore(reducer, compose(applyMiddleware(thunk, logger)));
